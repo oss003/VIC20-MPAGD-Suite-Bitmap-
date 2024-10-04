1752d7881a27aeb3e9834c5dcdf26a6959cc4d3a
Kees van Oss <k.van.oss@uticon.nl>
2024-10-04 10:53:38 +0200

    4-10-2024 Updated build and fixed joystick bug

--- build.bat
+++ build.bat
@@ -13,6 +13,13 @@
  if "%5"=="ntsc" set model=0
  if "%6"=="ntsc" set model=0
 
+ set turbo=0
+ if "%2"=="turbo" set turbo=1
+ if "%3"=="turbo" set turbo=1
+ if "%4"=="turbo" set turbo=1
+ if "%5"=="turbo" set turbo=1
+ if "%6"=="turbo" set turbo=1
+
 rem Compile AGD file
  copy scripts\%1.agd agd
  if errorlevel 1 goto error
@@ -35,10 +42,16 @@ rem Create discimage and add pictures + fastloader to diskimage
  echo Creating diskimage %1.d64
  cd ..\GTK3VICE-3.8-win64\bin
  c1541 -format "diskimage,id" d64 %1.d64 
- c1541 -attach %1.d64 -write ..\TAPES\%1.prg agdgame.prg
- c1541 -attach %1.d64 -write ..\..\CC65\turbodisk turbodisk 
  c1541 -attach %1.d64 -write ..\..\MUSIC\player1 player1 
- c1541 -attach %1.d64 -write ..\..\CC65\loader.prg %1.prg 
+
+ if %turbo%==1 (
+   c1541 -attach %1.d64 -write ..\TAPES\%1.prg agdgame.prg
+   c1541 -attach %1.d64 -write ..\..\CC65\turbodisk turbodisk 
+   c1541 -attach %1.d64 -write ..\..\CC65\loader.prg %1.prg 
+ )
+ if %turbo%==0 (
+   c1541 -attach %1.d64 -write ..\TAPES\%1.prg %1.prg
+ )
  for %%f in (..\..\pictures\%1*.*) do c1541 -attach %1.d64 -write %%f %%~nf
  move %1.d64 ..\discs >nul
 
